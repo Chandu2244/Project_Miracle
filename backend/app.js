ef779690c959ea0e2430277e9ddaac430ef1d14c
Y Chandra Sekhar Reddy <nani.chandu2015@gmail.com>
2025-12-09 14:56:12 +0530

    added Ip addess

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const dbInitializeAndServer = async () => {
     // Render gives port through environment variable
     const PORT = process.env.PORT || 3000;
 
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (e) {
